60263b8c243125ed6a7246bd78ae300e644630ba
jassi2000 <jaspreetactsingh@gmail.com>
2021-05-11 00:58:37 +0530

    some code issues fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import './App.css';
-import MediaCard from "./components/News"
-//import Lumsum from "./components/Calculators/Lumsum"
-import Search from "./components/Search"
-// import Swing from "./components/shortandswing/swing"
-// import Short from "./components/shortandswing/shortterm" 
-// import Intraday from "./components/intraday"
-// import MarketCap from "./components/MarketCap"
-import Pdfviewer from "./components/Pdfviewer"
 import Link from "./components/Link"
-import Book1 from "./components/Pdfviewer/Books/Book1"
 function App() {
   return (
     <div className="App">
-        {/* <MediaCard/> */}
-        {/* <Lumsum/> */}
-        {/* <Search /> */}
-        {/* <Swing/> */}
-        {/* <Short /> */}
-        {/* <Intraday/> */}
-        {/* <MarketCap/> */}
-        {/* <Pdfviewer /> */}
         <Link/>
     </div>
   );
